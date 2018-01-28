4ac1d0254961a18b4c4bcb8433d9f8f7e1ac0361
Karmoq <steffen.d.eichler@stud.h-da.de>
2018-01-28 15:10:01 +0100

    Final Game

--- GGJ 18 Tower Defense/Assets/Steve/EnvironmentManager.cs
+++ GGJ 18 Tower Defense/Assets/Steve/EnvironmentManager.cs
@@ -12,10 +12,12 @@ public class EnvironmentManager : MonoBehaviour {
     {
         //SpawnPrefab(TemplePrefab, new Vector3(-3, 0, TileManager.singleton.gridSize.y / 2 * TileManager.singleton.TileSize.y));
         //SpawnPrefab(SlaughterHousePrefab, new Vector3(TileManager.singleton.gridSize.x * TileManager.singleton.TileSize.x + 3, 0, TileManager.singleton.gridSize.y / 2 * TileManager.singleton.TileSize.y));
-
-        for (float x = -12; x < TileManager.singleton.gridSize.x * TileManager.singleton.TileSize.x * 2; x += 3.5f)
+        for (int s = 0; s < 3; s++)
         {
-                SpawnPrefab(CanyonPrefabs[Random.Range(0, CanyonPrefabs.Count)], new Vector3(x, 0, TileManager.singleton.gridSize.y * TileManager.singleton.TileSize.y +3));
+            for (float x = -12; x < TileManager.singleton.gridSize.x * TileManager.singleton.TileSize.x * 2; x += 3.5f)
+            {
+                SpawnPrefab(CanyonPrefabs[Random.Range(0, CanyonPrefabs.Count)], new Vector3(x + s + Random.Range(-0.05f, 0.05f), s*1, TileManager.singleton.gridSize.y * TileManager.singleton.TileSize.y + 3 +s*2 + Random.Range(-1f, +1)));
+            }
         }
     }
 
